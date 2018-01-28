f115c23ca4cd936f213c8580b77b74532db5f3a0
Ivan <mynamekasatkin@gmail.com>
2018-01-28 21:27:48 +0300

    21:30 IVAN

---
+++ python/robot.py
@@ -0,0 +1,13 @@
+
+def lerrning():
+	print('123')
+
+
+def main():
+	print('Ochko')
+
+
+
+
+if __name__ == '__main__':
+	main()
--- robot.sh
+++ robot.sh
@@ -3,7 +3,7 @@
 while (true)
 do
 echo "I get raning ROBOT..."
-cd '/home/'$USER'/Документы/PRIMETOOLS/python/lerning/'
+cd '/home/'$USER'/Документы/common/python/learning/'
 python3 robot.py
  sleep 0.5; # пауза 5 секунд
  clear
