e287dbd854b38e41847d18499986b4ca40552fc7
PRIMETOOLS <laptev.andrey@bk.ru>
2018-02-02 14:19:03 +0300

    ANDREY 14:18

--- xlsxProject/back-end/extractXlsx.php
+++ xlsxProject/back-end/extractXlsx.php
@@ -4,26 +4,30 @@ $argname = $argv[1];
 #$mysqli = new mysqli("localhost","root","","test");
 
 #open CSV
-$fh = fopen("../dataXlsx/export_.csv", "r");
+$fh = fopen("../dataXlsx/dataExport.csv", "r");
 #open CSV
 
 while ($line = fgetcsv($fh, 15795, ","))
 {
-    $code = $line[0];
-    $product = $line[1];
-    $articyl = $line[2];
-    $nameTovars = $line[3];
-    $opis = $line[4];
-    $opt = $line[5];
+    $code = $line[1];
+    $product = $line[2];
+    $articyl = $line[3];
+    $nameTovars = $line[4];
+    $opis = $line[5];
+    $optF = $line[6];
+    $optT = $line[7];
+    $optTU = $line[8];
+    $optO = $line[9];
+
 
     // Insert the data into the sales table
-    //$query = "INSERT INTO tovars SET id='".$code."',
+    //$query = "INSERT INTO goods SET id='".$code."',
     //    client_id='".$product."', order_time='".$articyl."',
 	//    sub_total='".$nameTovars."', shipping_cost='".$nal."',
     //    total_cost='".$opt."'";
 
     #$result = $mysqli->query($query);
-    echo $product. PHP_EOL;
+    echo $optT. PHP_EOL;
 }
 
 #Заносим данные о последнем изменении в лог
